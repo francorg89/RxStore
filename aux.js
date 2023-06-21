1999547fd77949473a1bc47a72f3eee1e8cc006c
Francisco Corro <francorg@Franciscos-MacBook-Pro.local>
2023-06-21 14:06:38 -0600

    basic site done

---
+++ aux.js
@@ -0,0 +1,14 @@
+let model = document.getElementById("model");
+let portatil = document.getElementById("portatil");
+
+
+//setTimeout(()=>{model.innerHTML="a webo"},2000);
+
+
+
+ let changeName = name => {model.innerHTML=name;};
+
+ let goTo = name => {document.location = "./models.html";}
+
+ let index = () => {document.location = "./index.html"}
+
